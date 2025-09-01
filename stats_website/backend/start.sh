7426fc799788b954c067d43967247e7cbe20f281
“georgemagnuss” <igeorgemattar@gmail.com>
2025-09-01 22:27:23 +0300

    fixmillion

--- stats_website/backend/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python manage.py migrate --noinput
-python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@test.com', 'admin123')"
-gunicorn --bind 0.0.0.0:8000 stats_backend.wsgi:application
