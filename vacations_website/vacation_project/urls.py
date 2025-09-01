69ed255295985497dd245d52ee8b31c78b65b776
“georgemagnuss” <igeorgemattar@gmail.com>
2025-09-01 19:15:34 +0300

    fix vacations1

--- vacations_website/vacation_project/urls.py
+++ vacations_website/vacation_project/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('', include('vacations.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve static and media files in production
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
